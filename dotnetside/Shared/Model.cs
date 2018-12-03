60e17a636ed2f8eea5cf75a2b5d62df45239b6a3
Fabio Cozzolino <cozzolino.fabio@gmail.com>
2018-12-03 17:26:54 +0100

    style update

---
+++ dotnetside/Shared/Model.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace dotnetside.Shared
+{
+
+    class Event
+    {
+        public string description { get; set; }
+        public DateTime end_time { get; set; }
+        public string name { get; set; }
+        public Place place { get; set; }
+        public DateTime start_time { get; set; }
+        public string id { get; set; }
+    }
+
+    class Place
+    {
+        public string name { get; set; }
+        public Location location { get; set; }
+        public string id { get; set; }
+    }
+
+    class Location
+    {
+        public string city { get; set; }
+        public string country { get; set; }
+        public int latitude { get; set; }
+        public int longitude { get; set; }
+        public string street { get; set; }
+        public string zip { get; set; }
+    }
+}
