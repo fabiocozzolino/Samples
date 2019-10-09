453af6cd4a635b8a3d8a187b37137b2cdebfb9d6
fabiocozzolino <cozzolino.fabio@gmail.com>
2019-10-09 21:11:58 +0200

    gRPC first sample

---
+++ gRPC.Samples/gRPC.Bidirectional.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Grpc.Core;
+
+namespace gRPC.Bidirectional.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
+            var client = new gRPC.Bidirectional.Core(channel);
+
+            // YOUR CODE GOES HERE
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
