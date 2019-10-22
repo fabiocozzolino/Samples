4033eb72fba3e45f085f840be52898cfcdd91bcf
fabiocozzolino <cozzolino.fabio@gmail.com>
2019-10-22 21:49:56 +0200

    gRPC Bidirectional Server

--- gRPC.Samples/gRPC.Bidirectional.Client/Program.cs
+++ gRPC.Samples/gRPC.Bidirectional.Client/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using gRPC.Bidirectional.Core;
+using GRPC.Bidirectional.Core;
 using Grpc.Core;
 
 namespace gRPC.Bidirectional.Client
@@ -10,33 +10,52 @@ namespace gRPC.Bidirectional.Client
     {
         static void Main(string[] args)
         {
-            Run();
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            Run().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine(task.Exception.ToString());
+                    foreach (var item in task.Exception.InnerExceptions)
+                    {
+                        Console.WriteLine("--- INNER");
+                        Console.WriteLine(item.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Finished!");
+                }
+            });
             Console.ReadLine();
         }
 
         async static Task Run()
         { 
-            var requests = new DoRequest[]
+            var requests = new DoMessage[]
             {
-                new DoRequest(){In = "First"},
-                new DoRequest(){In = "Second"},
-                new DoRequest(){In = "Third"},
-                new DoRequest(){In = "4"},
-                new DoRequest(){In = "5"},
-                new DoRequest(){In = "6"},
-                new DoRequest(){In = "7"},
-                new DoRequest(){In = "8"},
-                new DoRequest(){In = "9"},
-                new DoRequest(){In = "10"},
-                new DoRequest(){In = "11"}
+                new DoMessage(){In = "First"},
+                new DoMessage(){In = "Second"},
+                new DoMessage(){In = "Third"},
+                new DoMessage(){In = "4"},
+                new DoMessage(){In = "5"},
+                new DoMessage(){In = "6"},
+                new DoMessage(){In = "7"},
+                new DoMessage(){In = "8"},
+                new DoMessage(){In = "9"},
+                new DoMessage(){In = "10"},
+                new DoMessage(){In = "11"}
             };
 
-            Channel channel = new Channel("127.0.0.1:5001", ChannelCredentials.Insecure);
-            var client = new gRPC.Bidirectional.Core.MyService.MyServiceClient(channel);
-
+            Channel channel = new Channel("localhost", 50051, ChannelCredentials.Insecure);
+            var client = new MyService.MyServiceClient(channel);
             Console.WriteLine("Starting...");
 
-            using (var call = client.Do(new CallOptions()))
+            var options = new CallOptions().WithDeadline(DateTime.UtcNow.AddSeconds(15)).WithWaitForReady(true);
+            using (var call = client.Do(options))
             {
                 var responseReaderTask = Task.Run(async () =>
                 {
@@ -45,14 +64,25 @@ namespace gRPC.Bidirectional.Client
                     {
                         Console.WriteLine("Waiting...");
                         var note = call.ResponseStream.Current;
-                        Console.WriteLine("Received " + note.Out);
+                        Console.WriteLine("Received " + note.In);
                     }
                     Console.WriteLine("End Received");
+                }).ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("SERVER: " + task.Exception.ToString());
+                        foreach (var item in task.Exception.InnerExceptions)
+                        {
+                            Console.WriteLine("--- INNER SERVER");
+                            Console.WriteLine(item.ToString());
+                        }
+                    }
                 });
 
-                Thread.Sleep(500);
+
                 Console.WriteLine("Sending");
-                foreach (DoRequest request in requests)
+                foreach (DoMessage request in requests)
                 {
                     Console.WriteLine("Sending " + request.In);
                     await call.RequestStream.WriteAsync(request);
---
+++ gRPC.Samples/gRPC.Bidirectional.Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GRPC.Bidirectional.Core;
+
+namespace gRPC.Bidirectional.Remote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            //var features = RouteGuideUtil.ParseFeatures(RouteGuideUtil.DefaultFeaturesFile);
+
+            Server server = new Server
+            {
+                Services = { MyService.BindService(new MyServiceServer()) },
+                Ports = { new ServerPort("127.0.0.1", 50051, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("RouteGuide server listening on port " + 50051);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MyServiceServer : MyService.MyServiceBase
+    {
+        public override async Task Do(IAsyncStreamReader<DoMessage> requestStream, IServerStreamWriter<DoMessage> responseStream, ServerCallContext context)
+        {
+            ////var raceDuration = TimeSpan.Parse(context.RequestHeaders.Single(h => h.Key == "race-duration").Value);
+
+            //// Read incoming messages in a background task
+            //DoMessage? lastMessageReceived = null;
+            //var readTask = Task.Run(async () =>
+            //{
+            //    await foreach (var message in requestStream.ReadAllAsync())
+            //    {
+            //        lastMessageReceived = message;
+            //    }
+            //});
+
+            //// Write outgoing messages until timer is complete
+        
+            //await responseStream.WriteAsync();
+            
+
+            //await readTask;
+
+            while (await requestStream.MoveNext())
+            {
+                var note = requestStream.Current;
+                await responseStream.WriteAsync(new DoMessage { In = note?.In + "_reply" });
+    
+            }
+        }
+    }
+}
